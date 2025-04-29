51a46e8deafd5f8a02c2869c10da44b315c5ed3d
Kinyanjuipeter <voicesshekinah@gmail.com>
2025-04-29 11:29:49 +0300

    new

--- electronics_shop/wsgi.py
+++ electronics_shop/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/5.1/howto/deployment/wsgi/
 """
 
 import os
-
+import logging
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'electronics_shop.settings')
 
-application = get_wsgi_application()
+logger = logging.getLogger(__name__)
+logger.info('Starting WSGI application initialization')
+
+try:
+    application = get_wsgi_application()
+    logger.info('WSGI application initialized successfully')
+except Exception as e:
+    logger.error('Failed to initialize WSGI application: %s', str(e))
+    raise
+
+logger.info('Application is ready to serve requests')
